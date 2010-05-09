91740658e8ffec37dc16b8d3e11f400f2552a587
Givaldo de Cidra <contato@givaldodecidra.mus.br>
2010-05-09 15:34:04 -0300

    alteracoes disilva

--- consumo.py
+++ consumo.py
@@ -3,24 +3,48 @@
 
 from __future__ import print_function
 
-kmI = float(raw_input("Digite a kilometragem inicial: ")) ## pega o valor da kilometragem inicial do carro
-kmF = float(raw_input("Digite a kilometragem Final: ")) ## pega o valor da kilometragem final do carro
-vl = float(raw_input("Digite o valor do combustível: ")) ## pega o valor do combustível
-l = float(raw_input("Digite a quantidade de litros: ")) ## pega a quantidade em litros de combustível
+# há uma regra em Python (pep8) de não usar mais do que 80 caracteres
+# em uma linha. veja como fica mais enxuto quebrando a linha:
 
-r = kmF - kmI  # calcula a kilometragem rodada
+# melhor usar nomes de variáveis que tenham algum significado. o
+# entendimento de kmI ou r não é tão imediato quanto km_inicial e
+# km_rodados
 
-valorpago = vl *l # valor pago pelo combustível
+## pega o valor da kilometragem inicial do carro
+km_inicial = float(raw_input("Digite a kilometragem inicial: "))
 
-consumo = r / l  #calcula o consumo do carro
+## pega o valor da kilometragem final do carro
+km_final = float(raw_input("Digite a kilometragem Final: "))
+
+## pega o valor do combustível
+valor = float(raw_input("Digite o valor do combustível: "))
+
+## pega a quantidade em litros de combustível
+litros = float(raw_input("Digite a quantidade de litros: "))
+
+# calcula a kilometragem rodada
+km_rodados = km_final - km_inicial
+
+# valor pago pelo combustível
+valor_pago = valor * litros
+
+#calcula o consumo do carro
+consumo = km_rodados / litros
 
 print("Informações:")
-print('Kilometragem Inicial:', kmI,'km')
-print('Kilometragem Final:', kmF,'km')
-print('Valor do combustível: R$', vl)
-print('Quantidade de Lítros de combustível:', l,'lt')
+
+# print('Kilometragem Inicial:', km_inicial, 'km')
+# print('Kilometragem Final:', km_final, 'km')
+# print('Valor do combustível: R$', valor)
+# print('Quantidade de Lítros de combustível:', litros, 'lt')
+
+# compara com a formatação de strings que usei. para saber mais
+# procure por python string format no google
+
+print("Quilometragem inicial: {0} km\nQuilometragem final: {1} km".format(km_inicial, km_final))
+print("Valor do combustível: R$ {0}\nLitros de combustível: {1} l".format(valor, litros))
 
 print("Resultados:")
-print('Valor do Combustível: R$', valorpago)
-print('O carro rodou: ', r, 'km')
+print('Valor do Combustível: R$', valor_pago)
+print('O carro rodou: ', km_rodados, 'km')
 print('O carro está fazendo: ', consumo, 'Km/lt')
