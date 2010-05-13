f464c446fb34aabbea3cc18fe1526a7817fa5c6e
Givaldo de Cidra <contato@givaldodecidra.mus.br>
2010-05-12 21:48:22 -0300

    melhoramento de busca

--- filtra-nota.py
+++ filtra-nota.py
@@ -7,11 +7,12 @@ import re
 # notes_regex = re.compile("[a-g]")
 
 # lista-de-notas = "c cis d dis e f fis g gis a ais b"
-l = "ees"
+l = "[a-g]"
 
 with open('teste.ly', 'r') as f:
     p = f.read()
     # n = re.findall(r"\w+is", p)
     # n = re.findall(r"\w+", p)
+    # n = re.findall("[a-g]", p)
     n = re.findall(l, p)
     print(n)
